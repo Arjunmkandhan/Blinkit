ab28380a5a9a3c8232fe89fb268fc712792ce078
arjunmkandhan <kanarjunmkandhan200@gmail.com>
2026-03-01 21:19:29 +0530

    Updated the run blinker bat file

--- run_blinker.bat
+++ run_blinker.bat
@@ -1,2 +1,4 @@
-start pythonw blink_overlay.py
+cd /d "%~dp0"
+start pythonw blinker.py 
+
 @REM (Using pythonw instead of python ensures the script runs silently without opening a command prompt window.) 
